00e37dd8cf82f046d0ef0b195bc5fc739df9d5cb
likecheng <likecheng@vip.qq.com>
2016-11-25 16:07:58 +0800

    2016年11月25日16:07:56

--- sqlite/dataContextSqlite.ts
+++ sqlite/dataContextSqlite.ts
@@ -153,21 +153,3 @@ interface PropertyFormatResult {
     PropertyValueList: any[];
 }
 
-export function Transaction(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<Function>) {
-    let method = descriptor.value;
-    // descriptor.value = async function () {
-    //     console.log("BeginTranscation propertyName:", propertyName);
-    //     this.ctx.BeginTranscation();
-    //     let result;
-    //     try {
-    //         result = await method.apply(this, arguments);
-    //         this.ctx.Commit();
-    //         return result;
-    //     } catch (error) {
-    //         console.log("RollBack propertyName:", propertyName);
-    //         await this.ctx.RollBack();
-    //         throw error;
-    //     }
-    // }
-}
-
