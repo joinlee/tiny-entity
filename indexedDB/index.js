3fbf9f6fe4c587e47d227d67f605fcfa94181dad
likecheng <likecheng@vip.qq.com>
2016-11-29 22:33:55 +0800

    2016年11月29日22:33:53

--- indexedDB/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./dataContextIndexedDB'));
-__export(require('./entityObjectIndexedDB'));
-//# sourceMappingURL=index.js.map
--- sqlite/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./dataContextSqlite'));
-__export(require('./entityObjectSqlite'));
-//# sourceMappingURL=index.js.map
